bfd06ee31dd77386209b375228012e60237604c1
witampanstwa <robert.dziubek5@gmail.com>
2020-09-19 17:55:12 +0200

    Implement strict, ASCII matching

--- src/main/java/pl/witampanstwa/WordScrape/DataParser.java
+++ src/main/java/pl/witampanstwa/WordScrape/DataParser.java
@@ -7,11 +7,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Processes collected objects / collections of objects
+ * Processes collected objects / collections of objects.
+ * If street's key equals the number's key, these belong to the same record.
  */
 public class DataParser {
-    private int whatIndexMatchedAt;
-    private int whereIndexMatchedAt;
 
     public DataParser(List<Building> itemsLookedFor, List<Building> itemsLookedThrough) {
         List<List<String>> streetsLookedFor = itemsLookedFor.stream()
@@ -26,8 +25,36 @@ public class DataParser {
         List<List<String>> numbersLookedThrough = itemsLookedThrough.stream()
                 .map(Building::getNumbers)
                 .collect(Collectors.toList());
+
+        for (Pair<Integer, Integer> streetsPair : getStreetIntersections(streetsLookedFor, streetsLookedThrough)) {
+            for (Pair<Integer, Integer> numbersPair : getNumberIntersections(numbersLookedFor, numbersLookedThrough)) {
+                if (streetsPair.getKey().equals(numbersPair.getKey())
+                        && streetsPair.getValue().equals(numbersPair.getValue())) {
+                    System.out.println("\nmatched: ");
+                    System.out.println(streetsLookedFor.get(streetsPair.getKey()));
+                    System.out.println(numbersLookedFor.get(numbersPair.getKey()));
+
+                    System.out.println("with: ");
+                    System.out.println(streetsLookedThrough.get(numbersPair.getValue()));
+                    System.out.println(numbersLookedThrough.get(numbersPair.getValue()));
+
+                    System.out.println("at: ");
+                    System.out.println(streetsPair);
+                    System.out.println(numbersPair);
+                }
+            }
+        }
     }
 
+    /**
+     * Can return less/more than record count as there can be zero / multiple matches in the target row array--
+     * don't rely on the row count since this point.
+     *
+     * @param streetsLookedFor     streets part of the source input
+     * @param streetsLookedThrough streets part of the target input
+     * @return scattered source-target number-part-only relations, independent from the street part and therefore
+     * not tied to a row as a whole
+     */
     private List<Pair<Integer, Integer>> getStreetIntersections(List<List<String>> streetsLookedFor,
                                                                 List<List<String>> streetsLookedThrough) {
         List<Pair<Integer, Integer>> matchingTuples = new ArrayList<>();
@@ -70,13 +97,5 @@ public class DataParser {
         }
 
         return matchingTuples;
-    }
-
-    public int getWhatIndexMatchedAt() {
-        return whatIndexMatchedAt;
-    }
-
-    public int getWhereIndexMatchedAt() {
-        return whereIndexMatchedAt;
     }
 }
